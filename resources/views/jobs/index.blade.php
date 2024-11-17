63927e5e5eee5e23e862b698bf39009ae859843a
ralf <items@freenet.de>
2024-11-17 19:48:05 +0100

    first commit

---
+++ resources/views/jobs/index.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Job Listings</title>
+</head>
+
+<body>
+    <h1>
+        {{$title}}
+    </h1>
+    <ul>
+        @forelse ($jobs as $job)
+        <li>
+            {{$job}}
+        </li>
+        @empty
+        No jobs available
+        @endforelse
+    </ul>
+
+</html>
