6d29ecef341c560a97441c1e2ca059edb7366405
ralf <items@freenet.de>
2024-11-22 12:57:10 +0100

    dsas

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,25 +4,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.0/css/all.min.css"
-        integrity="sha512-9xKTRVabjVeZmc+GUW8GgSmcREDunMM+Dt/GrzchfN8tkwHizc5RP4Ok/MXFFy5rIjJjzhndFScTceq5e6GvVQ=="
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
+        integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
     @vite('resources/css/app.css')
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <script src="//unpkg.com/alpinejs" defer></script>
     <title>{{ $title ?? 'Workopia | Find and list jobs' }}</title>
 </head>
 
 <body class="bg-gray-100">
-    <x-header>
-        @if (request()->is('/'))
-            <x-hero />
-            <x-top-banner />
-        @endif
-        <main class="container mx-auto p-4 mt-4">
-            {{ $slot }}
-        </main>
-        <script src="{{ asset('js/script.js') }}"></script>
+    <x-header />
+    @if (request()->is('/'))
+        <x-hero />
+        <x-top-banner />
+    @endif
+    <main class="container mx-auto p-4 mt-4">
+        {{ $slot }}
+    </main>
+    <script src="{{ asset('js/script.js') }}"></script>
 </body>
 
 </html>
